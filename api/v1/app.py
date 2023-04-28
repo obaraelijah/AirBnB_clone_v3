e1170ec66813973ad1c692abb41ed0590b2b94d6
maiyo008 <tonymaiyo008@gmail.com>
2023-04-28 12:29:11 +0000

    Get status of API

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import Flask
 from models import storage
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,4 +15,6 @@ def clean_up(exception=None):
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=500, threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
